5d610c178e8945e191d31b2c34428c639cba8077
yuseon <llys0331@gmail.com>
2023-01-30 14:27:42 +0900

    initial commit

---
+++ src/main/java/jpabook/model/JpaMain.java
@@ -0,0 +1,43 @@
+package jpabook.model;
+
+import jpabook.model.entity.Order;
+import jpabook.model.entity.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        //엔티티 매니저 팩토리 생성
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager(); //엔티티 매니저 생성
+
+        EntityTransaction tx = em.getTransaction(); //트랜잭션 기능 획득
+
+        try {
+
+            tx.begin(); //트랜잭션 시작
+            //TODO 비즈니스 로직
+            Order order = new Order();
+
+            em.persist(order);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
+
+            tx.commit();//트랜잭션 커밋
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback(); //트랜잭션 롤백
+        } finally {
+            em.close(); //엔티티 매니저 종료
+        }
+
+        emf.close(); //엔티티 매니저 팩토리 종료
+    }
+}
